e2155b8544174cf979c95d3dd1a624fef4dc2b73
Abeerahmedg <112552899+Abeerahmedg@users.noreply.github.com>
2022-10-05 21:16:17 +0200

    Update Operations.cs

--- NewCalcolator/Operations.cs
+++ NewCalcolator/Operations.cs
@@ -1,54 +1,129 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using static NewCalcolator.Operations;
 
-namespace NewCalcolator
+double AskForNumber()
 {
-    public class Operations
+    double num;
+    Console.WriteLine("Please enter a number:");
+    num = Convert.ToDouble(Console.ReadLine());
+    return num;
+}
+decimal AskForNumbers()
+{
+    decimal num;
+    Console.WriteLine("Please enter a number:");
+    num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+
+do
+{
+    double num1;
+    double num2;
+    int num3;
+    int num4;
+    decimal resultt;
+    double result;
+
+
+    Console.WriteLine("Select Operation: ");
+    Console.WriteLine("+:Add\t-:Substract\t*:Multibly\t/:Divide");
+
+    switch (Console.ReadLine())
     {
-        public static double Addition(double num1, double num2)
-        {
-            return num1 + num2;
-        }
-        public static double Addition(int[] arr)
-        {
+        case "+":
+            Console.WriteLine("Choose the number you want: 1- Add two numbers.\n2- add more than two numbers");
+            int answer = Convert.ToInt32(Console.ReadLine());
 
-            return arr.Sum();
-        }
-        public static double Subtract(double num1, double num2)
-        {
-            return num1 - num2;
-        }
-        public static double Subtract(int[] arr)
-        {
-            double num = arr[0];
-            for (int i = 1; i < arr.Length; i++)
+            if (answer == 1)
             {
-                num -= arr[i];
-            }
-            return num;
-
-        }
-        public static double Multiply(double num1, double num2)
-        {
-            return num1 * num2;
-        }
-        public static decimal Divide(decimal num1, decimal num2)
-        {
-            decimal result = 0;
-            try
-            {
-                result = num1 / num2;
-            }
-            catch (DivideByZeroException)
-            {
-                Console.WriteLine("Can not be divided");
+                num1 = AskForNumber();
+                num2 = AskForNumber();
+                result = Addition(num1, num2);
+                Console.WriteLine($"The Result : {num1} + {num2} = " + result);
 
             }
-            return result;
-        }
+            else if (answer == 2)
+            {
+                Console.WriteLine("How many numbers would you like to get the sum for !");
+                int count = int.Parse(Console.ReadLine());
+                int[] arr = new int[count];
+                for (int i = 0; i < count; i++)
+                {
+                    Console.WriteLine("number{0}:", i + 1);
+                    arr[i] = int.Parse(Console.ReadLine());
+                }
+                result = Addition(arr);
+                Console.WriteLine($"The result of your numbers = {result}");
+
+            }
+            else Console.WriteLine("Wrong input!");
+            break;
+
+
+        case "-":
+            Console.WriteLine("Choose the number you want: \n1- Substract two number.\n2- Substract more than two");
+            int answer1 = Convert.ToInt32(Console.ReadLine());
+            if (answer1 == 1)
+            {
+                num1 = AskForNumber();
+                num2 = AskForNumber();
+                result = Subtract(num1, num2);
+                Console.WriteLine($"The Result: {num1} - {num2} = " + result);
+            }
+            else if (answer1 == 2)
+            {
+                Console.WriteLine("How many numbers would you like to get the sum for !");
+                int count = int.Parse(Console.ReadLine());
+                int[] arr = new int[count];
+                for (int i = 0; i < count; i++)
+                {
+                    Console.WriteLine("number{0}:", i + 1);
+                    arr[i] = int.Parse(Console.ReadLine());
+                }
+                result = Subtract(arr);
+                Console.WriteLine($"The result of your numbers = {result}");
+
+            }
+            else Console.WriteLine("Wrong input");
+
+            break;
+        case "*":
+            num1 = AskForNumber();
+            num2 = AskForNumber();
+            result = Multiply(num1, num2);
+            Console.WriteLine($"The Result: {num1} * {num2} =" + result);
+            break;
+        case "/":
+
+           num3 = (int)AskForNumbers();
+            num4 = (int)AskForNumbers();
+            if (num4 != 0) try //added if here to recogoniz the 0 input
+                {
+                    resultt = (decimal)Divide(num3, num4);
+                    Console.WriteLine($"The Result: {num3} / {num4}= " + resultt);
+                }
+                catch (DivideByZeroException msg)
+                {
+                    Console.WriteLine(msg);
+
+                }
+            else Console.WriteLine("Can not be divided!");
+            break;
+        default:
+            Console.WriteLine("That was not valid option");
+            break;
 
     }
+    Console.WriteLine("Should we continue!(Press Y for Yes and N for No): ");
+} while (Console.ReadLine().ToUpper() == "Y"); // the check condition for the loop
+
+Exception DivideByZeroException()
+{
+    throw new NotImplementedException();
+    Console.WriteLine("Can not be divided!");
 }
+
+Console.WriteLine("See You again!");
+Console.ReadKey();
+Environment.Exit(0);
